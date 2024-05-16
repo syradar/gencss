b333cc4e2339e87a78ceff2b7d52e32221dfd7b9
David Malmström <syntex@outlook.com>
2024-05-16 21:04:51 +0200

    add jsdoc to trace

--- utils/type/trace.js
+++ utils/type/trace.js
@@ -1,3 +1,8 @@
+/**
+ * Utility function to trace values in a functional pipeline.
+ * @param {Function} fn - The function to call with the current value.
+ * @returns {Function} - A function that takes a value, calls `fn` with it, and returns the value.
+ */
 export function trace(fn) {
   return (val) => {
     fn(val)
