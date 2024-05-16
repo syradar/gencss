9b7c94c92643d3eed36f7cb5dcead8b16fe54f97
David Malmström <syntex@outlook.com>
2024-05-16 21:19:24 +0200

    move css generation to separate file

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-import { EOL } from "node:os"
 import { styleText } from "node:util"
 import { getArgs } from "./utils/cli/arrrgs.js"
 import { readConfig } from "./utils/cli/config.js"
-import CSSUtils from "./utils/css/css.js"
-import { GenCSSLogger, LOG_LEVEL } from "./utils/cli/logger.js"
-import Numbers from "./utils/type/numbers.js"
-import Result from "./utils/type/result.js"
-import TypeCheck from "./utils/type/typeCheck.js"
 import { safeWriteFileSync } from "./utils/cli/files.js"
+import { GenCSSLogger } from "./utils/cli/logger.js"
+import { generateCssClasses } from "./utils/css/css.js"
 
 const args = getArgs()
 if (args.err) {
@@ -26,130 +22,18 @@ const logger = new GenCSSLogger({ logger: console.log, useColor, logLevel })
 const config = readConfig({ configPath, logger })
 
 if (config.err) {
-  logger.error(config.val)
+  logger.error(...config.val)
   process.exit(1)
 }
 
 const safeConfig = config.safeUnwrap()
 
-const ALLOWED_UNITS = ["rem", "px", "em"]
+const result = generateCssClasses(safeConfig)
 
-const spacing = safeConfig.spacing
-const spacing_validated = Object.entries(spacing).reduce(
-  (acc, [key, value]) => {
-    const num_and_unit = validateNumberAndUnit(value)
-
-    if (num_and_unit.ok) {
-      acc[key] = num_and_unit.unwrap()
-    }
-
-    return acc
-  },
-  {}
-)
-
-const breakpoints = safeConfig.breakpoints
-const breakpoints_validated = Object.entries(breakpoints).reduce(
-  (acc, [key, value]) => {
-    const num_and_unit = validateNumberAndUnit(value)
-
-    if (num_and_unit.ok) {
-      acc[key] = num_and_unit.unwrap()
-    }
-
-    return acc
-  },
-  {}
-)
-
-const PADDING = {
-  p: "padding",
-  pt: "padding-top",
-  pb: "padding-bottom",
-  pl: "padding-left",
-  pr: "padding-right",
-  pin: "padding-inline",
-  pbl: "padding-block",
-}
-
-const MARGIN = {
-  m: "margin",
-  mt: "margin-top",
-  mb: "margin-bottom",
-  ml: "margin-left",
-  mr: "margin-right",
-  min: "margin-inline",
-  mbl: "margin-block",
-}
-
-const SPACING_PROPERTIES = [PADDING, MARGIN]
-
-// Validate spacing values
-const result = SPACING_PROPERTIES.map((spacing_property) =>
-  Object.entries(spacing_validated).map(([spacer, { value, unit }]) => {
-    const generated_spacings = generate(
-      spacing_property,
-      breakpoints_validated,
-      spacer,
-      value,
-      unit
-    )
-    return generated_spacings
-  })
-)
-  .flat(3)
-  .join(EOL)
-
-try {
-  writeFileSync(outputPath, result + EOL, { encoding: "utf-8" })
-  logger.info(`Generated CSS written to: ${outputPath}`)
-} catch (err) {
-  logger.error(`${err}`)
-}
-
-/**
- * Validates a number and its CSS unit.
- * @param {string} spacer_value - The CSS value to validate.
- */
-function validateNumberAndUnit(spacer_value) {
-  return CSSUtils.splitUnit(spacer_value)
-    .andThen((splat) => {
-      const result = Numbers.safeParseFloat(splat.value).map((num) => ({
-        value: num,
-        unit: splat.unit,
-      }))
-
-      return result
-    })
-    .andThen((numUnit) => {
-      const isUnitlessZero =
-        numUnit.value === 0 && TypeCheck.isEmpty(numUnit.unit)
-      const isValidUnit = ALLOWED_UNITS.includes(numUnit.unit)
-
-      if (!isValidUnit && !isUnitlessZero) {
-        return Result.Err(`Invalid CSS unit: "${numUnit.unit}"`)
-      }
-
-      return Result.Ok(numUnit)
-    })
 const writeResult = safeWriteFileSync(outputPath, result)
 if (writeResult.err) {
   logger.error(`Failed to write to file: ${err.message}`)
   process.exit(1)
 }
 
-function generate(property_family, breakPoints, spacer, num, unit) {
-  return Object.entries(property_family).map(([prefix, property]) => {
-    return Object.entries(breakPoints).map(([bp_prefix, bp_value]) => {
-      if (bp_value.value === 0) {
-        return `.${prefix}-${spacer} {${EOL}\t${property}: ${num}${unit};${EOL}}${EOL}`
-      }
-
-      const top = `@media screen and (min-width: ${bp_value.value}${bp_value.unit}) {${EOL}\t`
-      const rule = `.${prefix}-${bp_prefix}-${spacer} {${EOL}\t\t${property}: ${num}${unit};${EOL}\t}`
-      const bottom = `${EOL}}`
-
-      return top + rule + bottom
-    })
-  })
-}
+logger.info(`Generated CSS written to: ${outputPath}`)
--- utils/css/css.js
+++ utils/css/css.js
@@ -1,13 +1,101 @@
+import { EOL } from "node:os"
 import Result from "../type/result.js"
 import Strings from "../type/strings.js"
+import TypeCheck from "../type/typeCheck.js"
+import Numbers from "../type/numbers.js"
 
 const NUMBER_WITH_UNIT_REGEXP = /^([-.\d]+(?:\.\d+)?)(.*)$/
+const ALLOWED_UNITS = ["rem", "px", "em"]
+const PADDING = {
+  p: "padding",
+  pt: "padding-top",
+  pb: "padding-bottom",
+  pl: "padding-left",
+  pr: "padding-right",
+  pin: "padding-inline",
+  pbl: "padding-block",
+}
+const MARGIN = {
+  m: "margin",
+  mt: "margin-top",
+  mb: "margin-bottom",
+  ml: "margin-left",
+  mr: "margin-right",
+  min: "margin-inline",
+  mbl: "margin-block",
+}
+const SPACING_PROPERTIES = [PADDING, MARGIN]
+
+export function generateCssClasses({ spacing, breakPoints }) {
+  const validSpacing = validateSpacing(spacing)
+  const validBreakpoints = validateBreakpoints(breakPoints)
+  // Validate spacing values
+  const result = SPACING_PROPERTIES.map((spacingProperty) =>
+    Object.entries(validSpacing).map(([spacer, { value, unit }]) => {
+      const generatedSpacings = generate(
+        spacingProperty,
+        validBreakpoints,
+        spacer,
+        value,
+        unit
+      )
+      return generatedSpacings
+    })
+  )
+    .flat(3)
+    .join(EOL)
+
+  return result
+}
+
+/**
+ * Validates a number and its CSS unit.
+ * @param {string} spacerValue - The CSS value to validate.
+ */
+function validateNumberAndUnit(spacerValue) {
+  return splitUnit(spacerValue)
+    .andThen((splat) => {
+      const result = Numbers.safeParseFloat(splat.value).map((num) => ({
+        value: num,
+        unit: splat.unit,
+      }))
+
+      return result
+    })
+    .andThen((numUnit) => {
+      const isUnitlessZero =
+        numUnit.value === 0 && TypeCheck.isEmpty(numUnit.unit)
+      const isValidUnit = ALLOWED_UNITS.includes(numUnit.unit)
+
+      if (!isValidUnit && !isUnitlessZero) {
+        return Result.Err(`Invalid CSS unit: "${numUnit.unit}"`)
+      }
+
+      return Result.Ok(numUnit)
+    })
+}
+
+function generate(propertyFamily, breakPoints, spacer, num, unit) {
+  return Object.entries(propertyFamily).map(([prefix, property]) => {
+    return Object.entries(breakPoints).map(([bpPrefix, bpValue]) => {
+      if (bpValue.value === 0) {
+        return `.${prefix}-${spacer} {${EOL}\t${property}: ${num}${unit};${EOL}}${EOL}`
+      }
+
+      const top = `@media screen and (min-width: ${bpValue.value}${bpValue.unit}) {${EOL}\t`
+      const rule = `.${prefix}-${bpPrefix}-${spacer} {${EOL}\t\t${property}: ${num}${unit};${EOL}\t}`
+      const bottom = `${EOL}}`
+
+      return top + rule + bottom
+    })
+  })
+}
 
 /**
  * Splits a string into a numerical value and a unit.
  * @param {string} v - The string to split.
  */
-export function splitUnit(v) {
+function splitUnit(v) {
   if (Strings.isNullishOrEmptyOrWhitespace(v)) {
     return Result.Ok({ value: v, unit: "" })
   }
@@ -21,8 +109,30 @@ export function splitUnit(v) {
   return Result.Ok({ value: split[1].trim(), unit: split[2].trim() })
 }
 
-const CSSUtils = {
-  splitUnit,
+function validateSpacing(spacing) {
+  const result = Object.entries(spacing).reduce((acc, [key, value]) => {
+    const numUnit = validateNumberAndUnit(value)
+
+    if (numUnit.ok) {
+      acc[key] = numUnit.unwrap()
+    }
+
+    return acc
+  }, {})
+
+  return result
 }
 
-export default CSSUtils
+function validateBreakpoints(breakPoints) {
+  const result = Object.entries(breakPoints).reduce((acc, [key, value]) => {
+    const numUnit = validateNumberAndUnit(value)
+
+    if (numUnit.ok) {
+      acc[key] = numUnit.unwrap()
+    }
+
+    return acc
+  }, {})
+
+  return result
+}
